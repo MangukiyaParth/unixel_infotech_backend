48c52c2812636ced209583cbc7c02e066635394b
Parth Mangukiya <mparth141@gmail.com>
2024-12-11 18:50:13 +0530

    add simple log

--- api/hello.js
+++ api/hello.js
@@ -3,9 +3,11 @@ const router = express.Router();
 
 // Get User time by Id
 router.get('/', async (req, res)=>{
-    console.log("hello");
+    let year = (new Date()).getFullYear() + 1;
+    let weekoffDays = getDefaultOffDays(year);
+    console.log(weekoffDays);
     // res(`Hello from ${process.env.VERCEL_REGION}`);
-    // res.status(500).json({ message: "Hello"});
+    res.status(200).json({ message: "Hello"});
 });
 
 module.exports = router;
