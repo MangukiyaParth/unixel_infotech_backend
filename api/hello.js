e12ca607a825cd2f3233249bd82c56a7ef1fde0f
Parth Mangukiya <mparth141@gmail.com>
2024-12-11 16:34:12 +0530

    update cron

--- api/hello.js
+++ api/hello.js
@@ -1,3 +1,14 @@
-export function GET(request) {
-    return new Response(`Hello from ${process.env.VERCEL_REGION}`);
-}
+const express = require('express');
+const router = express.Router();
+var fetchuser = require('../middlewere/fetchuser');
+var dbUtils = require('../helper/index').Db;
+const multer = require('multer');
+const upload = multer();
+
+// Get User time by Id
+router.get('/', fetchuser, upload.none(), [], async (req, res)=>{
+    // res(`Hello from ${process.env.VERCEL_REGION}`);
+    res.status(500).json({ message: "Hello"});
+});
+
+module.exports = router;
