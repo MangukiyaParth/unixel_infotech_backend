1dcda398d4287ac76047a49ed1622f5e4613ccbd
Parth Mangukiya <mparth141@gmail.com>
2024-12-11 16:36:08 +0530

    remove middlewhere

--- api/hello.js
+++ api/hello.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const router = express.Router();
-var fetchuser = require('../middlewere/fetchuser');
 var dbUtils = require('../helper/index').Db;
 const multer = require('multer');
 const upload = multer();
 
 // Get User time by Id
-router.get('/', fetchuser, upload.none(), [], async (req, res)=>{
+router.get('/', upload.none(), [], async (req, res)=>{
     // res(`Hello from ${process.env.VERCEL_REGION}`);
     res.status(500).json({ message: "Hello"});
 });
