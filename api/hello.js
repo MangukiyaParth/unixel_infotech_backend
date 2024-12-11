3781d2c4eeae7266b78821627a94e4f0b0529951
Parth Mangukiya <mparth141@gmail.com>
2024-12-11 18:52:26 +0530

    solve error

--- api/hello.js
+++ api/hello.js
@@ -4,7 +4,23 @@ const router = express.Router();
 // Get User time by Id
 router.get('/', async (req, res)=>{
     let year = (new Date()).getFullYear() + 1;
-    let weekoffDays = getDefaultOffDays(year);
+    
+    var date = new Date(year, 0, 1);
+    while (date.getDay() != 0) {
+        date.setDate(date.getDate() + 1);
+    }
+    var days = [];
+    while (date.getFullYear() == year) {
+        var m = date.getMonth() + 1;
+        var d = date.getDate();
+        days.push(
+            (d < 10 ? '0' + d : d) + '/' +
+            (m < 10 ? '0' + m : m) + '/' +
+            year
+        );
+        date.setDate(date.getDate() + 7);
+    }
+    let weekoffDays = days;
     console.log(weekoffDays);
     // res(`Hello from ${process.env.VERCEL_REGION}`);
     res.status(200).json({ message: "Hello"});
