84df40631ba6e63f064bd53ec4756d168b5b1cd4
Parth Mangukiya <mparth141@gmail.com>
2024-12-11 17:54:34 +0530

    solve error

--- middlewere/cron_auth.js
+++ middlewere/cron_auth.js
@@ -1,7 +1,7 @@
 const cronAuth = (req,res, next) => {
     // Get user from JWT and add it into req object
     const token = req.header('auth-token');
-    console.log(req.headers.get('Authorization'));
+    console.log(req.header('Authorization'));
     console.log(process.env.CRON_SECRET);
     // if (req.headers.get('Authorization') !== `Bearer ${process.env.CRON_SECRET}`) {
     //     return res.status(401).end('Unauthorized');
