61ac928e5010711da1fd06a0cbccfeb66295d4a4
Parth Mangukiya <mparth141@gmail.com>
2024-12-11 18:19:21 +0530

    remove other details

--- api/hello.js
+++ api/hello.js
@@ -1,12 +1,8 @@
 const express = require('express');
 const router = express.Router();
-var dbUtils = require('../helper/index').Db;
-var cronAuth = require('../middlewere/cron_auth');
-const multer = require('multer');
-const upload = multer();
 
 // Get User time by Id
-router.get('/', cronAuth, upload.none(), [], async (req, res)=>{
+router.get('/', async (req, res)=>{
     // res(`Hello from ${process.env.VERCEL_REGION}`);
     res.status(500).json({ message: "Hello"});
 });
