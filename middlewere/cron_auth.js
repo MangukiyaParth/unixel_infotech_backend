53660783f48ee3e95da1eb3738e888853f64880a
Parth Mangukiya <mparth141@gmail.com>
2024-12-11 17:45:20 +0530

    add log in cron auth

--- middlewere/cron_auth.js
+++ middlewere/cron_auth.js
@@ -1,7 +1,7 @@
 const cronAuth = (req,res, next) => {
     // Get user from JWT and add it into req object
-    const token = req.header();
-    console.log(token);
+    const token = req.header('auth-token');
+    console.log(req);
     next();
 }
 
