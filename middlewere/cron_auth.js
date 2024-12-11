2a8b055b77cdbda9e009a298f82696e5d28d260a
Parth Mangukiya <mparth141@gmail.com>
2024-12-11 17:53:12 +0530

    add logs

--- middlewere/cron_auth.js
+++ middlewere/cron_auth.js
@@ -1,7 +1,11 @@
 const cronAuth = (req,res, next) => {
     // Get user from JWT and add it into req object
     const token = req.header('auth-token');
-    console.log(JSON.stringify(req.headers));
+    console.log(req.headers.get('Authorization'));
+    console.log(process.env.CRON_SECRET);
+    // if (req.headers.get('Authorization') !== `Bearer ${process.env.CRON_SECRET}`) {
+    //     return res.status(401).end('Unauthorized');
+    // }
     next();
 }
 
